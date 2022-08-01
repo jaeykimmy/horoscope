2f31410afab6bfe8385df02dcb6735bf016dffaf
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-08-01 16:21:41 -0400

    dynamic data for props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { TextField, Button } from "@mui/material";
 
 export default function Aztro() {
   const [json, setJson] = useState({});
+  const [sign, setSign] = useState("");
+  const [day, setDay] = useState("");
 
-  useEffect(() => {
+  const handleSign = (event) => {
+    event.preventDefault();
+  };
+  const searchSignDay = () => {
     axios
       .post("https://aztro.sameerkumar.website", null, {
         params: {
-          sign: "gemini",
-          day: "today",
+          sign: sign,
+          day: day,
         },
       })
-      // .then((response) => response.json())
       .then(function (response) {
         setJson(response.data);
       });
-  }, []);
+  };
 
   return (
     <div>
-      Current Date: {json.current_date} <br />
-      Compatibility: {json.compatibility} <br />
-      Lucky Number: {json.lucky_number} <br />
-      Lucky Time: {json.lucky_time} <br />
-      Color: {json.color} <br />
-      Date Range: {json.date_range} <br />
-      Mood: {json.mood} <br />
-      Description: {json.description} <br />
+      <>
+        <TextField onChange={(e) => setSign(e.target.value)} />
+        <TextField onChange={(e) => setDay(e.target.value)} />
+        <Button type="submit" onClick={searchSignDay}>
+          Submit
+        </Button>
+
+        <div>
+          Current Date: {json.current_date} <br />
+          Compatibility: {json.compatibility} <br />
+          Lucky Number: {json.lucky_number} <br />
+          Lucky Time: {json.lucky_time} <br />
+          Color: {json.color} <br />
+          Date Range: {json.date_range} <br />
+          Mood: {json.mood} <br />
+          Description: {json.description} <br />
+        </div>
+      </>
     </div>
   );
 }
